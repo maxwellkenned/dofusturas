fc85192047dd043ae5b7478fe88631f32ca4141d
Juan <jluchovs@outlook.com>
2018-09-27 23:36:07 -0400

    Modificando package e index

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebase: {
     apiKey: "AIzaSyDG9Z4mtuTHlQThOjnOHNcH9B7d8No5qfs",
     authDomain: "dofusturas.firebaseapp.com",
